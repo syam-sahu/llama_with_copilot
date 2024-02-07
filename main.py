83d1407f73f2f94d241673345c3b71f44556f4e3
syam-sahu <132860070+syam-sahu@users.noreply.github.com>
2024-02-07 14:02:52 +0000

    pipenv instead of poetry

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
+from dotenv import load_dotenv
+import os
 
+from llama_index import VectorStoreIndex, SimpleDirectoryReader
+
+
+load_dotenv()
+
+documents = SimpleDirectoryReader("data").load_data()
+index = VectorStoreIndex.from_documents(documents)
+
+query_engine = index.as_query_engine()
+response = query_engine.query("What did the author do growing up?")
+print(response)
